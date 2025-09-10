c7d7d01ff5af93952738ded50179601cea462b9c
Brooke Mahoney <brooke@brookemahoney.com>
2025-09-10 15:50:34 -0700

    Adds vite integration

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Brooke Mahoney</h1>
+
+      <main>
+
+        <section className="card">
+          <h2>Coding for the Web Since 1995</h2>
+
+          <h3>Expertise</h3>
+
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>PHP</li>
+          </ul>
+
+          <h3>Specialties</h3>
+
+          <ul>
+            <li>Drupal</li>
+            <li>React</li>
+          </ul>
+
+          <h3>External Links</h3>
+
+          <ul>
+            <li>
+              <a href="https://drupal.org/u/loopduplicate">Drupal.org</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/brooke-marie-mahoney/">Linked.in</a>
+            </li>
+          </ul>
+
+        </section>
+
+      </main>
+    </>
+  )
+}
+
+export default App
